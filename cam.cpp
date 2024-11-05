34169c58a9fb7f4569a1be14f85d8321f820d9be
bowen <bo.zeng@westwell-lab.com>
2024-11-05 01:06:49 -0800

    modify print format

--- cam.cpp
+++ cam.cpp
@@ -9,7 +9,7 @@
 #include <sys/ioctl.h>
 #include <sys/mman.h>
 #include <linux/videodev2.h>
-
+#include "ebd_data_type.h"
 #define DEVICE "/dev/video0" // 默认相机设备
 
 // 输出错误信息并退出
@@ -79,8 +79,59 @@ void *map_buffer(int fd, const struct v4l2_buffer &buffer) {
     return buffer_start;
 }
 
+
+void print_lep_ebd(const lep_ebd_t &ebd) {
+    printf("------------ leopard imaging_ebd Information ------------\n");
+    
+    // 打印每个结构体的字段
+    printf("Size: %u\n", ebd.size.size);
+
+    // 打印 prv_inf_e13_t
+    printf("Chip ID : %u\n", ebd.indiv_inf.chip_id_e13);
+
+    // 打印 framer_count_e37_t
+    printf("Framer Count: %u\n", ebd.frame_count.framer_count);
+
+    // 打印 framer_id_e41_t
+    printf("Framer ID: %u\n", ebd.frame_id.framer_id);
+
+    // 打印 lep_temp_e53_t
+    printf("Temperature 1: %f, Temperature 2: %f\n", (ebd.temper.temp1&0xfff)/16.0 - 50, (ebd.temper.temp2&0xfff)/16.0 - 50);
+
+    // 打印 lep_err_e61_t
+    printf("Error Code E61: %.8x, Error Code E66: %.2x\n", ebd.err_code.code_e61, ebd.err_code.code_e66);
+
+    // 打印 vol_e89_t
+    printf("Voltage High: %fv, Voltage Mid: %fv, Voltage Low: %fv\n", ebd.vol.vh * 1e-3, ebd.vol.vm * 1e-3, ebd.vol.vl * 1e-3);
+
+    // 打印 image_size_e189_t
+    printf("Image Width: %u, Image Height: %u\n", ebd.img_size.horizontal, ebd.img_size.vertical);
+
+    // 打印 drive_mode_e197_t
+    printf("Drive Mode: %u\n", ebd.drive_mode.mode);
+
+    // 打印 sync_mode_e201_t
+    printf("Sync Method: %u, Operation Mode: %u\n", ebd.sync_mode.method, ebd.sync_mode.op_mode);
+
+}
+
+void to_ebd_data(uint8_t *p_data, lep_ebd_t *p_ebd) {
+    memcpy(&p_ebd->size, p_data + EBD_SIZE - 1, sizeof(p_ebd->size));
+    memcpy(&p_ebd->indiv_inf, p_data + INDIVIDUAL_INF - 1, sizeof(p_ebd->indiv_inf));
+    memcpy(&p_ebd->frame_count, p_data + FRAME_COUNT - 1, sizeof(p_ebd->frame_count));
+    memcpy(&p_ebd->frame_id, p_data + FRAME_ID - 1, sizeof(p_ebd->frame_id));
+    memcpy(&p_ebd->temper, p_data + TEMPERATURE_INF - 1, sizeof(p_ebd->temper));   
+    memcpy(&p_ebd->err_code, p_data + ERR_CODE - 1, sizeof(p_ebd->err_code));  
+    memcpy(&p_ebd->vol, p_data + VOL - 1, sizeof(p_ebd->vol));        
+    memcpy(&p_ebd->img_size, p_data + IMAGE_SIZE - 1, sizeof(p_ebd->img_size));        
+    memcpy(&p_ebd->drive_mode, p_data + DRIVE_MODE - 1, sizeof(p_ebd->drive_mode));     
+    memcpy(&p_ebd->sync_mode, p_data + SYNC_MODE - 1, sizeof(p_ebd->sync_mode));                          
+    print_lep_ebd(*p_ebd);
+}
+
+
 // 读取图像并以16进制输出（指定行范围并输出字节编号）
-void capture_and_output(int fd, struct v4l2_buffer &buffer, void *buffer_start, uint32_t start_row, uint32_t end_row, uint32_t width) {
+void capture_and_output(int fd, struct v4l2_buffer &buffer, void *buffer_start, size_t fram_count) {
     if (ioctl(fd, VIDIOC_QBUF, &buffer) == -1) {
         perror_exit("Error queueing buffer");
     }
@@ -93,19 +144,31 @@ void capture_and_output(int fd, struct v4l2_buffer &buffer, void *buffer_start, 
         perror_exit("Error dequeuing buffer");
     }
 
+    for(uint32_t i = 0; i < fram_count; i++) {
     // 输出图像数据为16进制，按行输出，附加字节编号
     uint8_t *data = static_cast<uint8_t *>(buffer_start);
+
+    lep_ebd_t ebd = {0};
+    to_ebd_data(data, &ebd); 
+
+    // 输出前600个字节的16进制值
+    printf("------------ Image Data (First 600 bytes) ------------\n");
     uint32_t byte_position = 1;  // 字节位置从1开始
+    for (uint32_t i = 0; i < 600 && i < buffer.length; ++i) {
+        // 按16进制输出字节和位置
+        printf("[%03d] %.2X ", byte_position++, data[i]);
 
-    for (uint32_t row = start_row; row <= end_row; row++) {
-        uint32_t row_start_idx = row * width * 2;  // 每行的起始字节位置，UYVY每个像素2字节
-        uint32_t row_end_idx = row_start_idx + width * 2;
-
-        for (uint32_t i = row_start_idx; i < row_end_idx; i++) {
-            if (i % 16 == 0 && i != row_start_idx) std::cout << std::endl;
-            printf("[%04d] %02X ", byte_position++, data[i]);
+        // 每输出16个字节换行
+        if (byte_position % 16 == 1) {
+            printf("\n");
         }
-        std::cout << std::endl;
+    }
+
+    // 如果不足600字节，输出剩余部分
+    if (byte_position % 16 != 1) {
+        printf("\n");
+    }
+     printf("---------------------------------------------\n");
     }
 
     if (ioctl(fd, VIDIOC_STREAMOFF, &buffer.type) == -1) {
@@ -114,16 +177,15 @@ void capture_and_output(int fd, struct v4l2_buffer &buffer, void *buffer_start, 
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 6) {
-        std::cerr << "Usage: " << argv[0] << " <device> <width> <height> <start_row> <end_row>" << std::endl;
+    if (argc != 5) {
+        std::cerr << "Usage: " << argv[0] << " <device> <width> <height> <frame_count>" << std::endl;
         return EXIT_FAILURE;
     }
 
     const char *device = argv[1];
     uint32_t width = std::stoi(argv[2]);
     uint32_t height = std::stoi(argv[3]);
-    uint32_t start_row = std::stoi(argv[4]);
-    uint32_t end_row = std::stoi(argv[5]);
+    uint32_t frame_count_max = std::stoi(argv[4]);  // 新增参数：指定要读取的帧数
 
     // 打开设备
     int fd = open_device(device);
@@ -139,9 +201,9 @@ int main(int argc, char *argv[]) {
     // 映射缓冲区
     void *buffer_start = map_buffer(fd, buffer);
 
+    lep_ebd_t ebd_obj = { 0 };
     // 读取数据并以16进制输出，指定行范围和字节编号
-    capture_and_output(fd, buffer, buffer_start, start_row, end_row, width);
-
+    capture_and_output(fd, buffer, buffer_start, frame_count_max);
     // 释放资源
     munmap(buffer_start, buffer_count);
     close(fd);
---
+++ ebd_data_type.h
@@ -0,0 +1,104 @@
+#ifndef LEOPARD_EBD_DATA_H
+#define LEOPARD_EBD_DATA_H
+#include <stdint.h>
+
+
+typedef enum {
+    EBD_SIZE = 1,
+    INDIVIDUAL_INF = 13,
+    HOST_SELECT = 21,
+    FRAME_COUNT = 37,
+    FRAME_ID = 40,
+    FRAM_MASK = 43,
+    TEMPERATURE_INF = 53,
+    ERR_CODE = 61,
+    VOL = 89,
+    ANALOG_GAIN_INF = 121,
+    IMAGE_SIZE = 189,
+    DRIVE_MODE = 197,
+    SYNC_MODE = 201,
+    WHITE_BALANCE_INF = 249,
+}LEP_EBD_IDX_t;
+
+typedef struct {
+  uint16_t size;
+}ebd_size_e01_t;
+
+typedef struct {
+  uint16_t chip_id_e13;//e13-e14
+  uint8_t undef_e15[2];//e15-e16
+  uint8_t chip_id_e17;
+  uint8_t chip_id_e18;
+  uint16_t chip_id_e19;//e19-e20
+  //uint16_t sensor_type_e225;//e255-e266
+  //uint8_t chip_id_e226;//e266
+}prv_inf_e13_t;
+
+
+typedef struct {
+    uint32_t framer_count;
+}framer_count_e37_t;
+
+typedef struct {
+    uint16_t framer_id;
+}framer_id_e41_t;
+
+typedef struct {
+  uint16_t temp1;
+  uint16_t temp2;
+}lep_temp_e53_t;
+
+typedef struct {
+  uint32_t code_e61;
+  uint8_t code_e66;
+}lep_err_e61_t;
+
+typedef struct {
+    uint16_t vh;
+    uint16_t vm;
+    uint16_t vl;
+}vol_e89_t;
+
+typedef struct {
+    uint16_t sp1h_e121;
+    uint16_t sp1l_e123;
+    uint16_t sp2h_e125;
+    uint16_t sp2l_e127;
+}anl_gain_e121_t;
+
+typedef struct {
+  uint16_t horizontal;
+  uint16_t vertical;
+}image_size_e189_t;
+
+typedef struct {
+    uint8_t mode;
+}drive_mode_e197_t;
+
+typedef struct {
+    uint8_t method;//e201
+    uint8_t op_mode;
+}sync_mode_e201_t;
+
+typedef struct {
+ uint16_t cf0;//e249
+ uint16_t cf1;//e251
+ uint16_t cf2;//e253
+ uint16_t cf3;//e255
+}white_balance_e249_t;
+
+typedef struct {
+    ebd_size_e01_t size;
+    prv_inf_e13_t indiv_inf;
+    framer_count_e37_t frame_count;
+    framer_id_e41_t frame_id;
+    lep_temp_e53_t temper;
+    lep_err_e61_t err_code;
+    vol_e89_t vol;
+    image_size_e189_t img_size;
+    drive_mode_e197_t drive_mode;
+    sync_mode_e201_t sync_mode;
+}lep_ebd_t;
+
+
+#endif
